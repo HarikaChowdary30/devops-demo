4f4ada237c7b067b46958d570239cc2dd617dfcc
Harika Nekkanti <harikachowdarynekkanti@gmail.com>
2025-07-18 11:30:34 +0530

    Initial commit for DevOps demo

---
+++ src/main/java/com/group4/devopsdemo/App.java
@@ -0,0 +1,7 @@
+package com.group4.devopsdemo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, welcome to DevOps class! This is Group 4 presentation on demonstration of Continuous Development in DevOps using Maven tool.");
+    }
+}
